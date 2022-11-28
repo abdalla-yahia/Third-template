59aa4a4ddedfb7de63a111b16027c6e888009109
Abdalla Yahia Kamel <94574716+abdalla-yahia@users.noreply.github.com>
2022-11-28 04:01:25 +0200

    Edit PWA

--- js/main.js
+++ js/main.js
@@ -244,14 +244,3 @@ btn_default.addEventListener('click', () => {
 })
 
 
-//######################################
-
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-        navigator.serviceWorker
-        .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
-    })
-}
-//######################################
--- serviceworker.js
+++ serviceworker.js
@@ -1,13 +1,12 @@
-const staticAbdallaYahia = "";
+const staticAbdallaYahia = "Abdalla-v1";
 const assets =
     [
     "/",
     "/index.html",
     "/css/Elzero.css",
-    "/css/style.css",
     "/css/all.min.css",
     "/css/normalize.css",
-    "/js/app.js",
+    "/js/main.js",
     "/imgs/avatar-01.png.webp",
     "/imgs/avatar-02.png.webp",
     "/imgs/avatar-03.png.webp",
@@ -34,15 +33,15 @@ const assets =
     "/imgs/features-03.jpg.webp",
     "/imgs/gallery-01.png.webp",
     "/imgs/gallery-02.png.webp",
-    "/imgs/gallery-03.png.webp",
+     "/imgs/gallery-03.jpg.webp",
     "/imgs/gallery-04.png.webp",
-    "/imgs/gallery-05.png.webp",
+    "/imgs/gallery-05.jpg.webp",
     "/imgs/gallery-06.png.webp",
     "/imgs/hosting-advanced.png.webp",
     "/imgs/hosting-basic.png.webp",
     "/imgs/hosting-professional.png.webp",
     "/imgs/landing-image.png.webp",
-    "/imgs/landing.jpg.webp",
+ 
     "/imgs/megamenu.png.webp",
     "/imgs/skills.png.webp",
     "/imgs/stats.jpg.webp",
@@ -50,42 +49,60 @@ const assets =
     "/imgs/team-02.jpg.webp",
     "/imgs/team-03.jpg.webp",
     "/imgs/team-04.jpg.webp",
-    "/imgs/team-05.jpg.webp",
-    "/imgs/team-06.jpg.webp",
+    "/imgs/team-05.png.webp",
+    "/imgs/team-06.png.webp",
     "/imgs/team-07.jpg.webp",
     "/imgs/team-08.jpg.webp",
     "/imgs/video-preview.jpg.webp",
-    "/imgs/videos-01.jpg.webp",
+
     "/imgs/work-steps-1.png.webp",
     "/imgs/work-steps-2.png.webp",
     "/imgs/work-steps-3.png.webp",
     "/imgs/work-steps.png.webp",
-    "/images/icons/icon-192x192.png",
 
+    "/images/icons/icon-192x192.png",
     "/images/icons/icon-384x384.png",
-    "/images/icons/icon-512x512.png",
+
+    
     "/webfonts/fa-brands-400.eot",
-    "/webfonts/fa-brands-400.svg",
+
     "/webfonts/fa-brands-400.ttf",
     "/webfonts/fa-brands-400.woff",
     "/webfonts/fa-brands-400.woff2",
     "/webfonts/fa-regular-400.eot",
-    "/webfonts/fa-regular-400.svg",
+
     "/webfonts/fa-regular-400.ttf",
     "/webfonts/fa-regular-400.woff",
     "/webfonts/fa-regular-400.woff2",
-    "/webfonts/fa-solid-900.eot",
-    "/webfonts/fa-solid-900.svg",
-    "/webfonts/fa-solid-900.ttf",
+
     "/webfonts/fa-solid-900.woff",
     "/webfonts/fa-solid-900.woff2",
 ]
 
+
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticAbdallaYahia).then(cache => {cache.addAll(assets)})
-  )
-})
+    caches.open(staticAbdallaYahia).then(async (cache) => {
+    let ok
+    console.log('ServiceWorker: Caching files:', assets.length, assets);
+    try {
+      ok = await cache.addAll(assets);
+    } catch (err) {
+      console.error('sw: cache.addAll',err);
+      for await (let i of assets) {
+        try {
+          ok = await cache.add(i);
+        } catch (err) {
+          console.warn('sw: cache.add',i);
+        }
+      }
+    }
+
+    return ok;
+  }));
+
+  console.log('ServiceWorker installed');
+});
 
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
@@ -94,10 +111,3 @@ self.addEventListener("fetch", fetchEvent => {
     })
   )
 })
-
-
-        // {
-        //     "src": "/images/icons/icon-256x256.png",
-        //     "sizes": "256x256",
-        //     "type": "image/png"
-        // },
